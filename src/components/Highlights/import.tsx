69e7b2f68965dc665c6532ccbd2c141e03c5b7d2
Rob Rohan <rob@therohans.com>
2021-09-30 08:13:48 +1300

    Trying another way to set the correct id

--- src/components/Highlights/import.tsx
+++ src/components/Highlights/import.tsx
@@ -41,22 +41,23 @@ export const Import: React.FC = () => {
   const _import = async (h: Highlight) => {
     const user = await useUser();
     // Change the ID to include our username
-    const uploadHighlight = Object.assign({ id: `${user.username}:${h.id}` }, h);
+    // const uploadHighlight = Object.assign({}, h, { id: `${user.username}:${h.id}` });
+    h.id = `${user.username}:${h.id}`;
 
     // This is pretty hammer-esq...
     // Try to create the new highlight
     try {
-      await API.graphql(graphqlOperation(createHighlight, { input: uploadHighlight }));
+      await API.graphql(graphqlOperation(createHighlight, { input: h }));
     } catch (e) {
       console.warn(e);
-      // alert(JSON.stringify(e));
+      alert(JSON.stringify(e));
       // if it fails, try to update...
       try {
-        await API.graphql(graphqlOperation(updateHighlight, { input: uploadHighlight }));
+        await API.graphql(graphqlOperation(updateHighlight, { input: h }));
       } catch (err) {
         // if that fails, the meh
         console.error(err);
-        // alert(JSON.stringify(err));
+        alert(JSON.stringify(err));
       }
     }
   };
